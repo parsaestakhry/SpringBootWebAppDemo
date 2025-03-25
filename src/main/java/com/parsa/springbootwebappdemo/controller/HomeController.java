5dbe556049cc5a32963c00f21c647a0f2101d743
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-25 23:00:33 +0330

    refactor add method in HomeController to use @RequestParam for better parameter handling

--- src/main/java/com/parsa/springbootwebappdemo/controller/HomeController.java
+++ src/main/java/com/parsa/springbootwebappdemo/controller/HomeController.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -17,9 +18,9 @@ public class HomeController {
 
 
     @RequestMapping("add")
-    public String add(int num1, int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num, int num2, HttpSession session){
 
-        int result = num1 + num2;
+        int result = num + num2;
 
         session.setAttribute("result", result);
 
