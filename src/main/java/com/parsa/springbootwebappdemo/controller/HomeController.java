9ffbe3be2aae688550dfd4826db30b43e25ec0ae
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-25 22:53:54 +0330

    add add method to HomeController for summing two numbers and storing result in session

--- src/main/java/com/parsa/springbootwebappdemo/controller/HomeController.java
+++ src/main/java/com/parsa/springbootwebappdemo/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.parsa.springbootwebappdemo.controller;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,5 +13,23 @@ public class HomeController {
     public String Home(){
         System.out.println("Home method called!");
         return "index.jsp";
+    }
+
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest request, HttpSession session){
+        int num1=  Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
+        int result = num1 + num2;
+
+        session.setAttribute("result", result);
+
+        System.out.println(result);
+
+
+
+
+        System.out.println("Add method called");
+        return "result.jsp";
     }
 }
