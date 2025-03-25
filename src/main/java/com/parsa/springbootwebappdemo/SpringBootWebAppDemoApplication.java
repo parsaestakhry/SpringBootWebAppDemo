51f976fba24a5a1e64dd4628ac3549f617c13de7
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-25 20:15:12 +0330

    initialize Spring Boot web application with Maven wrapper and basic project structure

---
+++ src/main/java/com/parsa/springbootwebappdemo/SpringBootWebAppDemoApplication.java
@@ -0,0 +1,13 @@
+package com.parsa.springbootwebappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebAppDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/parsa/springbootwebappdemo/SpringBootWebAppDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parsa.springbootwebappdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebAppDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
