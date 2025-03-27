cc0c7b0fec09c4737c7aceed031a0fd31b4234ef
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-27 18:49:36 +0330

    refactor add method in HomeController to use @RequestParam for better parameter handling

--- src/main/java/com/parsa/springbootwebappdemo/controller/HomeController.java
+++ src/main/java/com/parsa/springbootwebappdemo/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.parsa.springbootwebappdemo.controller;
 
 
+import com.parsa.springbootwebappdemo.Alien;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -13,16 +16,16 @@ public class HomeController {
     @RequestMapping("/")
     public String Home(){
         System.out.println("Home method called!");
-        return "index.jsp";
+        return "index";
     }
 
 
     @RequestMapping("add")
-    public String add(@RequestParam("num1") int num, int num2, HttpSession session){
+    public String add(@RequestParam("num1") int num, int num2, Model model,ModelAndView modelAndView ){
 
         int result = num + num2;
 
-        session.setAttribute("result", result);
+        model.addAttribute("result", result);
 
         System.out.println(result);
 
@@ -30,6 +33,28 @@ public class HomeController {
 
 
         System.out.println("Add method called");
-        return "result.jsp";
+        return "result";
+    }
+
+
+    @RequestMapping("addAlien")
+    public String addAlient(@RequestParam("aid") int aid, @RequestParam("aname") String aname, ModelAndView modelAndView ){
+
+        Alien alien = new Alien();
+        modelAndView.addObject("alien", alien);
+
+        modelAndView.setViewName("result");
+
+        alien.setAid(aid);
+        alien.setAname(aname);
+
+
+
+
+
+
+
+        System.out.println("Add Alien method called");
+        return "result";
     }
 }
--- src/main/webapp/views/result.jsp
+++ src/main/webapp/views/result.jsp
@@ -1,14 +1,14 @@
 <%@page language="java"%>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="../style.css">
     <title></title>
 </head>
 <body>
 <h2>
     DuDUDUDu max verstaaapen
 </h2>
-<h2>result is: <%= session.getAttribute("result") %> </h2>
+<h2>result is: <%= session.getAttribute("alien") %> </h2>
 </body>
 
 </html>
